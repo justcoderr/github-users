acffb073bb5738b56899a7d62df4342c3c2a4a2d
aelhajji <elhajjiabdeljabar@gmail.com>
2021-10-19 07:50:33 +0100

    fix bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { PagenotfoundComponent } from './components/pages/pagenotfound/pagenotfo
 import { UserComponent } from './components/pages/user/user.component';
 
 const routes: Routes = [
-  { path: '/github-users', component:  HomeComponent}, //adding github users just for github hosting
+  { path: 'github-users', component:  HomeComponent}, //adding github users just for github hosting
   { path: 'github-users/about', component: AboutComponent },
   {path: 'github-users/user/:id', component:UserComponent},
   {path: 'github-users/404', component: PagenotfoundComponent},
